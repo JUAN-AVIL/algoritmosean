b9d3a0b9d7ef22752b18c77665514ccb26395130
JUAN-AVIL <144927304+JUAN-AVIL@users.noreply.github.com>
2023-09-22 10:44:23 -0500

    subida de ejercicio 9

---
+++ Ejercicio 9 .py
@@ -0,0 +1,39 @@
+#vamos a realizar un programa que realice una tupla compuesta por diferentes tipos de 
+#datos que incluyan variables adicionalmente que esten orientadios a realizar diferentes acciones
+#enfocados en generar resultados en esas variaciones
+
+#print ("Este es uin programa de tuplas  ")
+
+#n = [(1,4),(4,8)]
+#t = [("Juan",1),(34, "hogar")]
+
+#print (n,t)
+
+#res = (n ,t )
+
+#for n in res :
+ #   print (n)
+  #  if n == res :
+   #     print(res)
+
+#Localizar dentro de una tupla la posicion determinada por un operador de referencia como el :
+
+#A = ("Programa para determinar tuplas en una variable ", "casa " "variable ", [2,9 , 2022, ], " gato ")
+
+
+#print (A[2:3 ])
+
+#for a in True :
+ #   print (A)
+  #  if A == True :
+   #     print ("casa")
+
+A = ("casa","carro ","celular  ", "1,2,3,4,5" , [26,9,2004], " gato " , "loros", "cargador " )
+
+print (A[0:1] )
+print (A[2:4 ])
+print (A[3:9 ] )
+
+
+
+
