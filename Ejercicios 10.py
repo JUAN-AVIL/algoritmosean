6fb7ddaf4a09f12cfa4ef00f78f2e236d13c0806
JUAN-AVIL <144927304+JUAN-AVIL@users.noreply.github.com>
2023-09-25 10:52:42 -0500

    Add files via upload

---
+++ Ejercicios 10.py
@@ -0,0 +1,10 @@
+#este es un complemento para realizar datos compuestos
+#tiene que ver con diccionarios
+
+d1 = {
+    "Nombre " : "SARA" ,
+    "EDAD" : "19" ,
+    "DOCUMENTO DE IDENTIDAD": "1578400"
+    }
+
+print ("este es mi primer dicionario ", d1)
